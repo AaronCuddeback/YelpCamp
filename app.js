ab5a523e445a503601a03d37ebdc25cd398e5114
Aaron <aaron.cuddeback@gymratpacks.com>
2017-11-03 04:22:38 +0000

    set new mongodb url

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ var commentRoutes = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes = require("./routes/index");
 
-var url = process.env.DATABASEURL || "mongodb://localhost/yelp_camp_v12";  
+// var url = process.env.DATABASEURL || "mongodb://localhost/yelp_camp_v12"; 
+var url = process.env.DATABASEURL || "mongodb://opposition:yelpcamp@ds243335.mlab.com:43335/opp_yelpcamp"; 
 mongoose.connect(url);
 app.use(bodyParser.urlencoded({extended: true}));
 
